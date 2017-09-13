4650b3bb2354007b77ecbdf4548e889b72137238
budnij-den <budden@list.ru>
2017-09-13 22:40:37 +0400

    make some logic

--- barclass.rb
+++ barclass.rb
@@ -1,62 +1,61 @@
-#barclass.rb 
-#calling russian 
-#encoding: utf-8
-# if (Gem.win_platform?) 
-# 	Encoding.default_external = Encoding.find(Encoding.locale_charmap) 
-# 	Encoding.default_internal = __ENCODING__ 
-
-# 		[STDIN, STDOUT].each do |io| 
-# 		io.set_encoding(Encoding.default_external, Encoding.default_internal) 
-# 		end 
-# end 
+# encoding: utf-8
+#barclass.rb
 
 	#создаём базу напитков, хэш соответствующей им посуды
 	#предполагаем, что база напитков будет автономной, подгружаемой
 ALLDRINKS=[:apperol,:brendy,:chinzano,:distilled_spirit]
 GLASSES={apperol: :highball, brendy: :glass, chinzano: :wineglass,distilled_spirit: :shot}
+WEEK=[:monday,:tuesday,:wednesday,:thursday,:friday,:saturday,:sunday]
 
-#classing 
-class BarContent 
+module Party
+  #взаимодействие бармена и бара
+    #задаём "блюдо дня". пусть оно зависит от дня недели
+    def bar_supply
+      @bardrink = ALLDRINKS[Time.now.wday-4]
+    end
+end
+
+#classing
+class BarContent
+  include Party
 	attr_accessor :bardrink
+
 		def initialize
-			bardrink=''
-		end 
-	#задаём "блюдо дня"
-		def bar_supply 
-			bardrink = ALLDRINKS[rand(0..3)] 
-			glass = GLASSES[@bardrink] 
-			glass = @glass
-		end 
-end 
+			@bardrink = ALLDRINKS[rand(ALLDRINKS.size)]
+      @glass = GLASSES[@bardrink]
+		end
+end
 
 class Barmen
-	
+include Party
 	attr_accessor :name, :barmendrink
 
-	def initialize
+	def initialize name
 		@name=name
-		@barmendrink=barmendrink
 	end
 		#создаём "предпочтение бармена"
-		def barmens_choice 
-			@barmendrink=ALLDRINKS[rand(0..3)]
-		end 
+		def barmens_choice
+			@barmendrink=ALLDRINKS[rand(ALLDRINKS.size)]
+		end
 
-	#бармен случайным образом разбивает налитое
-		def barmens_shaking_hands 
-			puts "Ох ты ж псыж! Разбил #{@glass}!" if rand(0..4)==0 
-		end 
+#бармен случайным образом разбивает налитое
+		def barmens_shaking_hands
+      puts "Ох ты ж псыж! Разбил #{@glass}!" if rand(4)==0
+		end
 
-	#если то, что есть в баре, совпадает с выбором бармена, у него поднимается настроение
-		def barmens_mood 
-			puts "Йо-хо!! Прекрасный вечер!" if @bardrink==@barmendrink 
-			puts "Наливаю тебе #{@bardrink} в #{@glass}" 
-		end 
-end 
+#если то, что есть в баре, совпадает с выбором бармена, у него поднимается настроение
+		def barmens_mood
+			puts "Йо-хо!! Прекрасный вечер!" if @bardrink==@barmendrink
+			puts "Наливаю тебе #{@bardrink} в #{@glass}"
+		end
 
-module Party 
-	#взаимодействие бармена и бара
-end 
+end
 
+
+
+monday_bar=BarContent.new
+p monday_bar
 nick = Barmen.new("Nick")
+p nick
+nick.barmens_choice
 p nick
